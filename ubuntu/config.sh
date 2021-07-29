84359788a23aa063022d7cd27e501857294d868b
richard <randria@users.noreply.github.com>
2021-07-29 09:56:14 +0200

    Create config.sh

---
+++ ubuntu/config.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This script can be used to apply the complete configuration of this LICCiA Base System
+# to a virtual machine
+
+APP_REPO="https://github.com/ls2n/liccia-vm.git"
+APP_DIR="/opt/liccia/git/$APP_NAME"
+
+mkdir -p `dirname $APP_DIR` && git clone ${APP_REPO} ${APP_DIR} && {
+  cd ${APP_DIR}
+  source install.sh
+  source deploy.sh
+}
