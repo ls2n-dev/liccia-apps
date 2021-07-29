cc19b66f4a47166c4ca5e7f3c28aa9fb210f5940
richard <randria@users.noreply.github.com>
2021-07-29 10:00:49 +0200

    Update config.sh

--- ubuntu/config.sh
+++ ubuntu/config.sh
@@ -6,8 +6,9 @@
 APP_REPO="https://github.com/ls2n/liccia-vm.git"
 APP_DIR="/opt/liccia/git/$APP_NAME"
 
-mkdir -p `dirname $APP_DIR` && git clone ${APP_REPO} ${APP_DIR} && {
-  cd ${APP_DIR}
+mkdir -p `dirname $APP_DIR` && git clone ${APP_REPO} ${APP_DIR} && cd ${APP_DIR} && {
+  source profile_liccia.sh
+  echo "-- `date" > LICCIA_LOGFILE
   source install.sh
   source deploy.sh
 }
