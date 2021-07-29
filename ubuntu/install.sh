d3a5fd1dc797db27f931ff0d484f00da21e73ce1
richard <randria@users.noreply.github.com>
2021-07-29 09:58:07 +0200

    Create install.sh

---
+++ ubuntu/install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Inspired from https://gitlab.in2p3.fr/ifb-biosphere/apps/ubuntu-ifb
+# Hacked for LICCiA Environment
+
+# This script is executed on the virtual machine during the Installation phase (need to be ran as root!).
+# It is used to record a predefined VM-image of the appliance.
+# Otherwise executed first during a cloud deployement in OS-BIRD Infra
+
+# Install Ansible
+export DEBIAN_FRONTEND=noninteractive
+APT_OPT=""
+DIST_CODENAME=$(lsb_release -cs)
+if [ $(lsb_release -is) = "Debian" ]; then
+    apt-get install -y software-properties-common dirmngr
+	if [[ $DIST_CODENAME == "stretch" || $DIST_CODENAME == "buster" ]]; then
+        apt-add-repository "deb http://deb.debian.org/debian ${DIST_CODENAME}-backports main";
+    fi
+    APT_OPT="-t ${DIST_CODENAME}-backports --allow-unauthenticated"
+    apt-get -y --allow-unauthenticated dist-upgrade
+elif [ $DIST_CODENAME != "focal" ]; then
+    apt-add-repository -y ppa:ansible/ansible
+fi
+apt-get update
+apt-get install $APT_OPT -y ansible
+
+# Run app playbook
+ansible-playbook -c local -i 127.0.0.1, -b -e 'ansible_python_interpreter=/usr/bin/python3' system-update.yaml
