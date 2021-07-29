7fd46aebe37f8c11e194ce904fe4cc1e97332f9d
richard <randria@users.noreply.github.com>
2021-07-29 09:48:14 +0200

    Update profile_liccia.sh

--- ubuntu/profile_liccia.sh
+++ ubuntu/profile_liccia.sh
@@ -2,4 +2,5 @@
 export LICCIA_MAIN=/opt/liccia
 export LICCIA_BIN=$LICCIA_MAIN/bin
 export LICCIA_DATADIR=$LICCIA_MAIN/data
+export LICCIA_LOGFILE=$LICCIA_MAIN/liccia.log
 export PATH=$PATH:$LICCIA_BIN
