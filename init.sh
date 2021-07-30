fbdcc2068253da1dd9adcbd7286f3c9ffc73c8b3
richard <randria@users.noreply.github.com>
2021-07-30 14:07:12 +0200

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# initialize gitlab setup project environment
+#
+export GITLAB_SERVER="https://gitlab.univ-nantes.fr/api/v4"
+export GITLAB_TOKEN=$1
+export GITLAB_GROUP_ID=9147
+export GITLAB_PROJECT_ID=10925
+export GITLAB_FILE=setup_global.sh
+
+if [ -z "$GITLAB_TOKEN" ]; then
+  echo "*ERROR* Please set the GITLAB_TOKEN variable"
+  return 1
+fi
+
+# get global setup
+SOURCE=`mktemp`
+curl -sSLf --header "PRIVATE-TOKEN: $GITLAB_TOKEN" \
+     -XGET "${GITLAB_SERVER}/projects/$GITLAB_PROJECT_ID/repository/files/${GITLAB_FILE}/raw" 2>/dev/null > $SOURCE
+
+# source setup file
+source $SOURCE
+rm -f $SOURCE 2>/dev/null
