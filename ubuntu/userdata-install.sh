925de28db4bf53c76098ed80a7e885cf40d1e087
richard <randria@users.noreply.github.com>
2021-07-30 17:33:29 +0200

    Update userdata-install.sh

--- ubuntu/userdata-install.sh
+++ ubuntu/userdata-install.sh
@@ -2,9 +2,10 @@
 # called by ansible process - system-update.yaml
 #
 # Add Docker’s official GPG key
-echo \
-  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+#echo \
+#  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+#  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 usermod -aG docker ubuntu
 mkdir -p /etc/systemd/system/docker.service.d
