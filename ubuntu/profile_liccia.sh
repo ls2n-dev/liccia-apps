3cd140ed5bc6b312e28c77ba68b65defa52e983b
richard <randria@users.noreply.github.com>
2021-07-30 18:04:42 +0200

    Update profile_liccia.sh

--- ubuntu/profile_liccia.sh
+++ ubuntu/profile_liccia.sh
@@ -4,14 +4,3 @@ export LICCIA_BIN=$LICCIA_MAIN/bin
 export LICCIA_DATADIR=$LICCIA_MAIN/data
 export LICCIA_LOGFILE=$LICCIA_MAIN/liccia.log
 export PATH=$PATH:$LICCIA_BIN
-
-function liccia_log(){
-  TAG='INFO'
-  case "$1" in
-    INFO | CMD)  TAG=$1; shift ;;
-    ERROR) TAG="*ERROR*"; shift ;;
-  esac
-  echo "[`date +%Y-%m-%dT%T%z`] $TAG $*" | tee -a $LICCIA_LOGFILE
-}
-export -f liccia_log
-
