89233fbd68e04fef2d1c4b9835c4d401143c1a8b
richard <randria@users.noreply.github.com>
2021-07-31 11:20:46 +0200

    Update gitlab_curl.sh

--- gitlab_curl.sh
+++ gitlab_curl.sh
@@ -2,11 +2,12 @@
 
 # initialize gitlab setup project environment
 #
+
+[ -z "$GITLAB_TOKEN" ] && export GITLAB_TOKEN=$1 && shift
 export GITLAB_SERVER="https://gitlab.univ-nantes.fr/api/v4"
-export GITLAB_TOKEN=$1
 export GITLAB_GROUP_ID=9147
 export GITLAB_PROJECT_ID=10925
-export GITLAB_FILE=$2
+export GITLAB_FILE=$1
 
 if [ -z "$GITLAB_TOKEN" ]; then
   echo "*ERROR* Please set the GITLAB_TOKEN variable"
