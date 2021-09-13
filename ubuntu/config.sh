bf6257e9adc36072ac514b966e133d167f562bbd
richard <randria@users.noreply.github.com>
2021-09-13 10:00:49 +0200

    Update config.sh

--- ubuntu/config.sh
+++ ubuntu/config.sh
@@ -5,13 +5,13 @@
 
 APP_NAME=liccia-ubuntu
 APP_REPO="https://github.com/ls2n-dev/liccia-apps.git"
-APP_DIR="/opt/liccia/git/${APP_NAME}"
+APP_DIR="/opt/liccia"
+APP_GIT="${APP_DIR}/git/${APP_NAME}"
 
-mkdir -p `dirname $APP_DIR` && chown ubuntu: `dirname $APP_DIR` && git clone ${APP_REPO} ${APP_DIR}
-cd ${APP_DIR}/ubuntu && {
+mkdir -p `dirname $APP_GIT` && chown ubuntu: `dirname $APP_DIR` && git clone ${APP_REPO} ${APP_GIT} && \
+cd ${APP_GIT}/ubuntu && {
   source profile_liccia.sh
   source profile_funcs.sh
   liccia_log INFO "start configuration process"
   source install.sh
-  #source deploy.sh
 }
