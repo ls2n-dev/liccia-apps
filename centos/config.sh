ec5ba1b1a590ec788423f62dfd826105107f7e95
richard <richard.randriatoamanana@ec-nantes.fr>
2021-10-19 23:07:55 +0200

    add flavor centos

--- centos/config.sh
+++ centos/config.sh
@@ -3,13 +3,14 @@
 # This script can be used to apply the complete configuration of this LICCiA Base System
 # to a virtual machine
 
-APP_NAME=liccia-ubuntu
+APP_FLAVOR=centos
+APP_NAME=liccia-$APP_FLAVOR
 APP_REPO="https://github.com/ls2n-dev/liccia-apps.git"
 APP_DIR="/opt/liccia"
 APP_GIT="${APP_DIR}/git/${APP_NAME}"
 
-mkdir -p `dirname $APP_GIT` && chown ubuntu: `dirname $APP_DIR` && git clone ${APP_REPO} ${APP_GIT} && \
-cd ${APP_GIT}/ubuntu && {
+mkdir -p `dirname $APP_GIT` && chown ${APP_FLAVOR}: `dirname $APP_DIR` && git clone ${APP_REPO} ${APP_GIT} && \
+cd ${APP_GIT}/${APP_FLAVOR} && {
   source profile_liccia.sh
   source profile_funcs.sh
   liccia_log INFO "start configuration process"
