1596354d352059c6fa8dc0fcb75d47570c1a712c
richard <randria@users.noreply.github.com>
2021-10-25 11:45:49 +0200

    Update config.sh

--- centos/config.sh
+++ centos/config.sh
@@ -12,8 +12,8 @@ APP_GIT="${APP_DIR}/git/${APP_NAME}"
 yum -y install git
 mkdir -p `dirname $APP_GIT` && chown ${APP_FLAVOR}: `dirname $APP_DIR` && git clone ${APP_REPO} ${APP_GIT} && \
 cd ${APP_GIT}/${APP_FLAVOR} && {
-  source profile_liccia.sh
-  source profile_funcs.sh
+  source ./profile_liccia.sh
+  source ./profile_funcs.sh
   liccia_log INFO "start configuration process"
-  source install.sh
+  source ./install.sh
 }
