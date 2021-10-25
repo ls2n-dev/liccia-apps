f867bb573ff4d9561ff230f837d283e1e1daece8
richard <randria@users.noreply.github.com>
2021-10-25 11:56:02 +0200

    Update config.sh

--- centos/config.sh
+++ centos/config.sh
@@ -10,7 +10,7 @@ APP_DIR="/opt/liccia"
 APP_GIT="${APP_DIR}/git/${APP_NAME}"
 
 yum -y install git
-mkdir -p `dirname $APP_GIT` && chown ${APP_FLAVOR}: `dirname $APP_GIT` && git clone ${APP_REPO} ${APP_GIT} && chown -R ${APP_FLAVOR}: $APP_GIT && \
+chown ${APP_FLAVOR}: $APP_DIR && mkdir -p `dirname $APP_GIT` && git clone ${APP_REPO} ${APP_GIT} && chown -R ${APP_FLAVOR}: $APP_GIT && \
 cd ${APP_GIT}/${APP_FLAVOR} && {
   pwd
   source profile_liccia.sh
