9f6f1e9f5295a5eee4510dfc1c8ff15083be3516
richard <randria@users.noreply.github.com>
2021-10-27 15:35:54 +0200

    Update install-nvidia.sh

--- ubuntu/install-nvidia.sh
+++ ubuntu/install-nvidia.sh
@@ -7,10 +7,15 @@ apt autoremove*
 echo "++ update grub config"
 sed -i.bak-`date +%s` 's/^\(GRUB_CMDLINE_LINUX=\"\)\(.*\)\"/\1\2 nouveau.modeset=0\"/g' /etc/default/grub
 
-# reboot
+# reboot$
+
 DRIVER_VERSION=418.197.02
+echo "++ downloading nvidia-driver version $DRIVER_VERSION"
 if [[ `sudo find /opt/data -maxdepth 2 -iname 'NVIDIA-Linux*' | wc -l` -eq 0 ]]; then 
    BASE_URL=https://us.download.nvidia.com/tesla
    cd /opt/data/ && sudo curl -fSsl -O $BASE_URL/$DRIVER_VERSION/NVIDIA-Linux-x86_64-$DRIVER_VERSION.run
 fi
-[ -f /opt/data/NVIDIA-Linux-x86_64-$DRIVER_VERSION.run ]] && sh /opt/data/NVIDIA-Linux-x86_64-$DRIVER_VERSION.run -s -a
+[ -f /opt/data/NVIDIA-Linux-x86_64-$DRIVER_VERSION.run ]] && {
+   echo "+++ installing the driver..."
+   sh /opt/data/NVIDIA-Linux-x86_64-$DRIVER_VERSION.run -s -a
+}
