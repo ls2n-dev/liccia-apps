ca648e2e8507e0f3ee92ba0bcc4ad3ca514dd97b
richard <randria@users.noreply.github.com>
2021-11-01 20:47:02 +0100

    Update userdata-tljh.sh

--- ubuntu/userdata-tljh.sh
+++ ubuntu/userdata-tljh.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -lxe
+# Run without a sudo prefixed.
 #
-apt update && \
-apt install -y python3 python3-dev git curl wget && \
+sudo apt update && \
+sudo apt install -y python3 python3-dev git curl wget && \
 conda update -n base -c defaults conda && \
 curl -L https://tljh.jupyter.org/bootstrap.py | sudo -E python3 - --admin jupyadmin
